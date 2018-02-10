3ea72c409c4c922ba1b8e7744049711cf8f114fb
cdx <cdx@cdxdeMacBook-Pro.local>
2018-02-10 23:58:11 +0800

    Add SSM Demo

---
+++ SSM_Demo/src/main/java/org/cdx/ssm/controller/HelloWorldController.java
@@ -0,0 +1,20 @@
+package org.cdx.ssm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by cdx on 2018/1/28.
+ */
+// Controller注解,标识此类为Controller
+@Controller
+public class HelloWorldController {
+    //RequestMapping里面的值用于指定这个方法的访问路径,当url中request过来时会与RequestMapping的值进行匹配,匹配成功则交由对应的方法处理
+    @RequestMapping("/welcome")
+    public ModelAndView helloWorld() {
+        System.out.println("hello"); //打印信息,证明进行了当前方法
+        // 返回ModelAndView对象交由Spring MVC处理,第一个参数标识跳转的页面,第二个参数是设置的属性,第三个参数是属性的值
+        return new ModelAndView("welcome", "message", "Hello World!");
+    }
+}
---
+++ SSM_Demo/src/main/webapp/WEB-INF/jsp/welcome.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>${message} SSM Demo</h2>
+</body>
+</html>
